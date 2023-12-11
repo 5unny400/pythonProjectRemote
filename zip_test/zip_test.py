a985a05704cda6b13d2c5c15d219b72f7bd3663c
shenxinyuan <shenxinyuan@datagrand.com>
2023-12-11 10:12:02 +0800

    add some py test demo 3

---
+++ zip_test/zip_test.py
@@ -0,0 +1,12 @@
+"""
+@FileName：zip_test.py
+@Description：
+@Author：shenxinyuan
+@Time：2023/12/7
+"""
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']  # 使用zip函数将两个列表打包成元组的迭代器
+zipped = zip(list1, list2)  # 将迭代器转换为列表
+result_list = list(zipped)
+
+print(result_list)
