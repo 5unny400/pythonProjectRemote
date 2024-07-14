301460780b65d660893800af8ddf4e08668b58f2
shenxy <shenxy@mingyuanyun.com>
2024-07-14 11:58:16 +0800

    rename suffix of image

--- 2024_q2/rename_file_sufix/convert_webp_png.py
+++ 2024_q2/rename_file_sufix/convert_webp_png.py
@@ -3,7 +3,10 @@ import os
 
 
 def convert_webp_to_png(directory):
+    index = 1
+    cnt = 0
     for filename in os.listdir(directory):
+        cnt += 1
         if filename.endswith('.webp'):
             webp_file = os.path.join(directory, filename)
             png_file = os.path.join(directory, filename.replace('.webp', '.png'))
@@ -14,12 +17,15 @@ def convert_webp_to_png(directory):
                     img = img.convert('RGB')
                     # 保存为 PNG 格式的图片
                     img.save(png_file, 'PNG')
+                    print(str(index)+":"+png_file+"已修改为png格式！")
                 # 删除原始的 WebP 格式图片
                 os.remove(webp_file)
+                index += 1
             except Exception as e:
                 print(f"Error converting {webp_file}: {e}")
-
+    print(f"一共遍历了{cnt}张图片。")
 
 # 使用示例
-directory_path = 'D:\\Administrator\\图片'  # 修改为你的目录路径
+# directory_path = 'D:\\Administrator\\图片'  # 修改为你的目录路径
+directory_path = 'E:\\photos'  # 修改为你的目录路径
 convert_webp_to_png(directory_path)
